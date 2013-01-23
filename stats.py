77528e5cb67c98a872fb69f3e96bfb872adb12bd
Carlos Anderson <carlosja@msu.edu>
2013-01-23 03:22:21 -0500

    Use NumPy for stats.py

--- stats.py
+++ stats.py
@@ -1,60 +1,28 @@
 import random
+import numpy
 
 
 # Returns a random subset of size n from alist (without replacement)
 # Previous function name: randomSubset
 def sample(alist, n):
-
-  subsetIndeces = []
-
-  while n > 0:
-    index = random.randint(0, len(alist) - 1)
-    while index in subsetIndeces:
-      index = random.randint(0, len(alist) - 1)
-    subsetIndeces.append(index)
-    n -= 1
-
-  subset = [alist[i] for i in subsetIndeces]
-
-  return subset
+  return random.sample(alist, n)
 
 
 def mean(alist):
-
-  mean = 0.0
-  for item in alist:
-    mean += item
-
-  if len(alist) > 0:
-    mean /= len(alist)
-
-  return mean
+  return numpy.mean(alist)
 
 
 def var(alist):
-
-  average = mean(alist)
-
-  sum = 0.0
-  for item in alist:
-    sum += (item - average) * (item - average)
-  sum /= len(alist) - 1
-
-  return sum
+  return numpy.var(alist)
 
 
 def stdev(alist):
-  return pow(var(alist), 0.5)
+  return numpy.std(alist)
 
 
 def cov(alist, blist):
+  amean = mean(alist)
+  bmean = mean(blist)
 
-  average1 = mean(alist)
-  average2 = mean(blist)
-
-  sum = 0.0
-  for i in range(len(alist)):
-    sum += (alist[i] - average1) * (blist[i] - average2)
-  sum /= len(alist) - 1
-
-  return sum
+  return numpy.sum([(alist[i] - amean) * (blist[i] - bmean)
+    for i in range(len(alist))]) / float((len(alist) - 1))
